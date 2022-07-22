4cacb11a365d6881e3cfa43a0d3cc8fa0557f656
Krish Shah <shahkrish2016@gmail.com>
2022-07-21 23:32:51 -0400

    prettify json and format it alphabetically

--- article.py
+++ article.py
@@ -1,6 +1,19 @@
 
 import os
+import json
 
-for count, value in enumerate(os.listdir('data')):
-    os.rename('data/' + value, 'data/article-' + str(count+1) + '.json')
-    print(count, value)
+# file_data = open('data/article-108.json', 'w')
+# file_data.write(json.dumps(file_data, indent=4, sort_keys=True))
+
+files = sorted(os.listdir('data'), key=lambda x: int(x[x.find('-')+1:x.find('.')]))
+
+
+for file in files:
+    print(file)
+    with open('data/' + file, 'r+') as storage:
+        a = storage.read()
+        parsed = json.loads(a)
+        storage.seek(0)
+        storage.write(json.dumps(parsed, indent = 4, sort_keys=True))
+
+
