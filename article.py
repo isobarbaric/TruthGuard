8865f2ee9aaef377ee72af0c668928e606e572f2
Krish Shah <shahkrish2016@gmail.com>
2022-07-22 00:16:49 -0400

    cleaned up JSON

--- article.py
+++ article.py
@@ -4,17 +4,20 @@ import os
 
 unwanted = ['back_matter', 'bib_entries', 'ref_entries']
 
+# ensuring all data was in json format to begin with
+# to be done rn 
+
+# parsing and simplifying JSON data
 for file in os.listdir('data'):
-    print(file)
-    with open('data/' + file, 'r+') as f:
+    path = 'data/' + file
+    content = None
+    with open(path, 'r') as f:
         content = f.read()
-        current_paper = json.loads(content)
-        current_paper['metadata'] = {'paper_id': current_paper['paper_id'], 'title': current_paper['metadata']['title']}
-        del current_paper['paper_id']
-        for word in unwanted:
-            del current_paper[word]
-        f.seek(0)
+    current_paper = json.loads(content)
+    current_paper['metadata'] = {'paper_id': current_paper['paper_id'], 'title': current_paper['metadata']['title']}
+    del current_paper['paper_id']
+    for word in unwanted:
+        del current_paper[word]
+    with open(path, 'w') as f:
         f.write(json.dumps(current_paper, indent=4, sort_keys=True))
 
-# keys to drop: back_matter, bib_entries, ref_entries
-# keys to format, take meta_data and replace with key just containing its title attribute
