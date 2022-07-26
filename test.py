be95689c8734fb472ae9107ee386d6d786d5766c
Krish Shah <shahkrish2016@gmail.com>
2022-07-26 15:14:08 -0400

    improved cleaned up data

--- test.py
+++ test.py
@@ -3,20 +3,33 @@ import json
 from newspaper import Article
 
 # issues with (double slashes after name + repeating website name)
-# should print out failed webpage names later
-# 29/483 failed for conspiracy articles
-# 49/307 failed for science articles
+# first run:
+# - 29/483 failed for conspiracy articles
+# - 49/307 failed for science articles
+# second run:
+# - 20/481 failed for conspiracy articles
+# - 19/314 failed for science articles
 
 covid_articles = []
 not_working = 0
 
-with open('extracted/science.json', 'r') as f:
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+with open('extract/conspiracy.json', 'r') as f:
     json_str = json.loads(f.read())
     for index, article_ref in enumerate(json_str):
         print(index+1, '/', len(json_str))
         modified_link = article_ref[1] 
+
+        # remove double slash after first occurence
         if modified_link.find('http://') == -1 and modified_link.find('https://') == -1:
+            modified_link = rreplace(modified_link, '//', '/', modified_link.count('//'))
             modified_link = 'https://' + modified_link
+        else:
+            modified_link = rreplace(modified_link, '//', '/', modified_link.count('//')-1)
+
         try:
             rn = Article(modified_link)
             rn.download()
@@ -26,7 +39,7 @@ with open('extracted/science.json', 'r') as f:
             not_working += 1
             print(modified_link)
 
-with open('json/science.json', 'w') as f:
+with open('json_test/conspiracy.json', 'w') as f:
     f.write(json.dumps(covid_articles, indent = 4))
 
 print(not_working)
