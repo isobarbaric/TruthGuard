06c80277dd119e6c76a0123e3b386ee8c4ad48ef
Krish Shah <shahkrish2016@gmail.com>
2022-08-04 18:51:05 -0400

    a lot of clean up

---
+++ bagOfWords.py
@@ -0,0 +1,120 @@
+# creating a bag of words class for the bag of words model
+
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from dateutil.parser import parse
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+
+import string
+import nltk
+
+class BagOfWords:
+    def __init__(self, tokenized_paragraph: list, name: str):
+        self.sentences = tokenized_paragraph
+        self.name = name
+
+    def tokenize(self):
+        self.words = []
+        for sentence in self.sentences:
+            for word in word_tokenize(sentence):
+                self.words.append(word)
+
+    def to_lower_case(self):
+        for i in range(len(self.words)):
+            self.words[i] = self.words[i].lower()
+
+    def clean_data(self):
+        noise = ['...', "n't"]
+        def is_time_or_date(word):  
+            try:
+                parsed = parse(word)
+                return True
+            except:
+                return False
+
+        def is_link(word):
+            suffixes = ['.com', '.org', '.edu', '.gov', '.int', '.co', '.net', '.au', '.us', '.uk', '.ne', 'news']
+            for suffix in suffixes:
+                if suffix in word:
+                    return True
+            return False
+
+        for i in range(len(self.words)-1, -1, -1):
+            if len(self.words[i]) <= 2 or self.words[i].isnumeric() or is_time_or_date(self.words[i]) or self.words[i] in noise or is_link(self.words[i]) or self.words[i] in [letter for letter in string.ascii_lowercase]:
+                self.words.pop(i)
+                continue
+        
+            # shave punctation off of beginnings and from the end
+            start_ind, end_ind = -1, -1
+            for j in range(len(self.words[i])):
+                if self.words[i][j] in string.ascii_lowercase or self.words[i][j].isnumeric():
+                    start_ind = j
+                    break
+            for j in range(len(self.words[i])-1, -1, -1):
+                if self.words[i][j] in string.ascii_lowercase or self.words[i][j].isnumeric():
+                    end_ind = j
+                    break
+
+            if (start_ind == 0 and end_ind == len(self.words[i])-1) or start_ind >= end_ind:
+                continue
+
+            self.words[i] = self.words[i][start_ind:end_ind+1]
+
+    def remove_stop_words(self):
+        for i in range(len(self.words)-1, -1, -1):
+            if self.words[i] in stopwords.words('english'):
+                self.words.pop(i)  
+
+    def normalize_words(self):
+        def get_part_of_speech(provided_word):
+            _, part_of_speech = nltk.pos_tag([provided_word])[0]
+            if 'NN' in part_of_speech:
+                return 'n'
+            if 'VB' in part_of_speech:
+                return 'v'
+            if 'JJ' in part_of_speech:
+                return 'a'
+            if 'RB' in part_of_speech:
+                return 'r'
+            return 'n'
+
+        lemmatizer = WordNetLemmatizer()
+        for i in range(len(self.words)):
+            self.words[i] = lemmatizer.lemmatize(self.words[i], get_part_of_speech(self.words[i]))
+
+        # perform some data cleaning on lemmatized words
+        for i in range(len(self.words)-1, -1, -1):
+            if self.words[i] in [letter for letter in string.ascii_lowercase]:
+                self.words.pop(i)  
+
+    def create_frequency_chart(self):
+        self.freqChart = dict()
+
+        for word in self.words:
+            if word not in self.freqChart:
+                self.freqChart[word] = 1
+            else:
+                self.freqChart[word] += 1
+
+        # sorting in ascending order by value
+        self.freqChart = {word: self.freqChart[word] for word in sorted(self.freqChart, key=self.freqChart.get, reverse=True)}
+
+    def plot_frequency_chart(self):
+        words = list(self.freqChart.keys())[:100]
+        frequencies = list(self.freqChart.values())[:100]
+
+        plt.figure(figsize=(20, 5))
+        plt.margins(x=0, tight=True)
+        plt.bar(words, frequencies, color ='green')
+
+        # setting title and labels
+        plt.xlabel("Distinct Words")
+        plt.tick_params(axis='x', which='major', labelsize=9)
+        plt.xticks(rotation = 90)
+
+        plt.ylabel(f"Frequency of Words in {self.name}")
+        plt.title("Frequency Chart")
+
+        # loading the plot
+        plt.show()
--- tasks.md
+++ tasks.md
@@ -1,11 +1,29 @@
-- make sure rows have correct phrase associated with them, labelling should be accurate
-- split the data set after the first cell in a 80-20 data set
-- make note of different classification models to use and use them 
-- pass in word instead of boolean in bag of words constructor
-
 general parameters to vary
-- number of articles
-- test_size
-- number of relevant words
 
 objective is to have data_model take parameters to vary the general parameters
+
+
+make sure the use for each variable created is known
+- can remove variables that are never used 
+
+multiple changes to see how accuracy improves
+- parameters to vary inside the model
+    - model-specific: 
+        - hyperparameters on current model and create blackbox-able structure
+        - GridSearchCV, RandomizedGridSearchCV
+    - heuristic-specific: 
+        - number of articles
+        - test_size
+        - number of relevant words
+- try using libraries to compare model accuracy
+    - scikit-learn CountVectorizer
+    - spaCy
+    - TextBlob 
+- data source (CORD-19) and another potential source for the conspiracy articles
+
+things worked on:
+
+things ongoing:
+
+things going to be tried out:
+
