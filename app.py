4035f617a15cc5ff20e235b83fa67c0a1771a03a
Krish Shah <shahkrish2016@gmail.com>
2022-08-29 18:57:11 -0400

    fixed static folder path

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
 
-app = Flask(__name__, template_folder='Website/templates')
+app = Flask(__name__, template_folder='Website/templates', static_folder='Website/static')
 
 import API.api
 import Website.website
