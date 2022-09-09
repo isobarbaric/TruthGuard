87cfede80ab8c58b3b082237410ac10290687294
Krish Shah <shahkrish2016@gmail.com>
2022-09-09 14:02:24 -0400

    working on setting up results

--- Website/website.py
+++ Website/website.py
@@ -1,5 +1,18 @@
 
+from distutils.command.build import build
 from flask import render_template, request, Blueprint
+
+import json
+import sys
+import os
+import requests
+import pandas as pd
+import pickle
+
+# adding extra file path to get accesss to bag of words class
+sys.path.append(os.path.abspath(os.getcwd() + "/ML Pipeline"))
+
+from bag_of_words import BagOfWords
 
 website_blueprint = Blueprint(
     'website',
@@ -9,21 +22,63 @@ website_blueprint = Blueprint(
     static_url_path='/home-static'
 )
 
+def build_predict_dataframe(text_content):
+    with open('Data/model/relevant_words.json') as f:
+        relevant_words = json.loads(f.read())
+
+    current_test = BagOfWords(text_content, None)
+
+    cols = {}
+    for word in relevant_words:
+        cols[word] = [current_test.freq_chart[word] if word in current_test.freq_chart else 0]
+
+    data_set = pd.DataFrame(data = cols)
+
+    return data_set
+
+with open('API/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+def predict(article_body):
+    df = build_predict_dataframe(article_body)
+
+    prediction_number = model.predict(df)[0]
+    if prediction_number == 0:
+        prediction = 'conspiracy/pseudoscience'
+    else:
+        prediction = 'pro-science'
+
+    confidence_level = model.predict_proba(df)[0].tolist()
+
+    confidence_level = {
+        'pro-science': confidence_level[1],
+        'conspiracy/pseudoscience': confidence_level[0]
+    }
+
+    return {'prediction': prediction,
+            'confidence_level': confidence_level}
+
 @website_blueprint.route('/', methods=['GET', 'POST'])
 def home_page():
-    # print(request.method)
-    print(request.form)
-    article_link = request.form.get('article-link')
-    article_body = request.form.get('article-text')
-    print(article_body)
-    print(article_link)
+    if request.method == 'POST':
+        article_link = request.form.get('article-link')
+        article_body = request.form.get('article-text')
 
-    # print(request.form[0])
-    # if request.method == 'POST':
-        # print(request.form)
-        # a = request.form['article-link']
-        # print(a)
-        # if query-link, then process accordingly => have to distinguish between the two text boxes
-        # return render_template("result.html", text_content = a)
-    # else:
-    return render_template("index.html")
+        print(article_body)
+        print(article_link)
+
+        prediction = None
+
+        if article_link is not None:
+            try:
+                api_response = requests.get(f'http://covid19-classifier-app.herokuapp.com/API/{article_link}')
+                prediction = api_response.json()
+            except Exception as e:
+                prediction = e
+        elif article_body is not None:
+            # construct data in some way as the api response
+            prediction = predict(article_body)
+
+        return render_template("result.html", pred = prediction)
+    else:
+        return render_template('index.html')
